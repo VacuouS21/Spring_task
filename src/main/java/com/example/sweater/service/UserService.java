8bebc43445d579d5b57719db08ea7d3716d8f21c
VacuouS <vacuous21@gmail.com>
2021-11-14 23:39:45 +0300

    Update User

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,10 +1,12 @@
 package com.example.sweater.service;
 
+import com.example.sweater.Models.UserUpdateRequesModel;
 import com.example.sweater.entity.User;
 import com.example.sweater.repos.UserRep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -17,5 +19,15 @@ public class UserService {
     }
     public List<User> getUsers(){
         return userRep.findAll();
+    }
+
+    public User update(Long id, UserUpdateRequesModel userUpdateRequesModel) {
+    User user=userRep.findById(id).orElseThrow(EntityNotFoundException::new);
+    user.setTickets(userUpdateRequesModel.getTickets());
+    user.setFio(userUpdateRequesModel.getFio());
+    user.setUser_login(userUpdateRequesModel.getUser_login());
+    user.setUser_password(userUpdateRequesModel.getUser_password());
+    userRep.save(user);
+    return user;
     }
 }
